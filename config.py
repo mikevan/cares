cbc86a4e693a11fe93acff2da55e680870404753
mikevan <vangeertruym@gmail.com>
2026-01-09 14:20:56 -0600

    changed edition name to "Regalia - A CARES edition for the Knights of Columbus"

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ Set APP_NAME and DEFAULT_ORGANIZATION here, or override using instance/config.py
 
 class Config:
     # The visible application name shown in templates and generated reports
-    APP_NAME = "CARES - Community Accounting & Resource Engagement System"
+    APP_NAME = "Regalia - A CARES edition for Knights of Columbus"
 
     # Default organization/chapters used as report fallbacks and initial seed data
-    DEFAULT_ORGANIZATION = "CARES - Example Chapter"
+    DEFAULT_ORGANIZATION = "Knights of Columbus - Example Chapter"
 
     # You can add other configuration values here as needed

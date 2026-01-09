c36e79fc730b07b024f61bc1a5aa9a4089db68d5
mikevan <vangeertruym@gmail.com>
2026-01-09 14:45:44 -0600

    simplified naming

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ Set APP_NAME and DEFAULT_ORGANIZATION here, or override using instance/config.py
 
 class Config:
     # The visible application name shown in templates and generated reports
-    APP_NAME = "Regalia - A CARES edition for Knights of Columbus"
+    APP_NAME = "REGALIA"
 
     # Default organization/chapters used as report fallbacks and initial seed data
-    DEFAULT_ORGANIZATION = "Knights of Columbus - Example Chapter"
+    DEFAULT_ORGANIZATION = "A CARES Edition for the Knights of Columbus - Example Chapter"
 
     # You can add other configuration values here as needed
